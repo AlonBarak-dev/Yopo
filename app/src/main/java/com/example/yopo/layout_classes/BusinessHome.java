90825d0648c2704b07a2bb4ec819edd0fe42dfa8
alonbarak-dev <alonbarak3110@gmail.com>
2022-11-17 03:45:35 +0200

    Added Logout button in the Business Home page

--- app/src/main/java/com/example/yopo/layout_classes/BusinessHome.java
+++ app/src/main/java/com/example/yopo/layout_classes/BusinessHome.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -15,9 +16,22 @@ import com.example.yopo.R;
 
 public class BusinessHome extends AppCompatActivity {
 
+    private ImageButton logout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.business_home_layout);
+
+        logout = findViewById(R.id.logout_button_business);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Logout from the profile -> back to Main page
+                Intent i = new Intent(BusinessHome.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }

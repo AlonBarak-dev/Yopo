9f6899cb05c4457b12478878129763c9ae0f0d82
eyalhagai12 <eyalh2001@gmail.com>
2022-11-29 16:53:45 +0200

    connected business schedule to client from landing page

--- app/src/main/java/com/example/yopo/layout_classes/BusinessLandingPageActivity.java
+++ app/src/main/java/com/example/yopo/layout_classes/BusinessLandingPageActivity.java
@@ -2,9 +2,13 @@ package com.example.yopo.layout_classes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.yopo.R;
 import com.example.yopo.data_classes.Database;
@@ -15,6 +19,7 @@ public class BusinessLandingPageActivity extends AppCompatActivity {
     private Database database;
 
     private TextView business_name, business_description;
+    private Button schedule_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,10 +38,21 @@ public class BusinessLandingPageActivity extends AppCompatActivity {
         // get field variables
         business_name = findViewById(R.id.business_name);
         business_description = findViewById(R.id.business_description_text);
+        schedule_button = findViewById(R.id.schedule_button);
 
         // set fields to correct information
         // TODO use business name instead
         business_name.setText((String) business_data.get("username"));
         business_description.setText((String) business_data.get("description"));
+
+        // set event to button click
+        schedule_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(BusinessLandingPageActivity.this, ClientAppointmentScheduleActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 }

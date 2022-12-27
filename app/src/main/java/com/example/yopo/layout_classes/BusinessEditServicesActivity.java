42f4299865cd2a533d9d307a88d7e8b2c1d27c3f
Sahar Tuvyahu <sahar45456@gmail.com>
2022-12-27 18:18:09 +0200

    Added basic RecyclerItemClickListener

--- app/src/main/java/com/example/yopo/layout_classes/BusinessEditServicesActivity.java
+++ app/src/main/java/com/example/yopo/layout_classes/BusinessEditServicesActivity.java
@@ -2,6 +2,7 @@ package com.example.yopo.layout_classes;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -9,6 +10,7 @@ import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -17,6 +19,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.yopo.R;
 import com.example.yopo.data_classes.Database;
 import com.example.yopo.data_classes.Session;
+import com.example.yopo.util_classes.RecyclerTouchListener;
 import com.example.yopo.util_classes.Service;
 import com.example.yopo.util_classes.ServiceAdapter;
 
@@ -48,14 +51,14 @@ public class BusinessEditServicesActivity extends AppCompatActivity {
         List<Service> services = new ArrayList<>();
 
         if (session.get_session_attribute("username") == null) {
-            Log.wtf("BusinessEditServices", "Session: username is null!");
+            Log.e("BusinessEditServices", "Session: username is null!");
         }
         else {
             Log.v("BusinessEditServices", session.get_session_attribute("username").toString());
         }
 
         if (list_of_services == null) {
-            Log.wtf("BusinessEditServices", "list_of_services is null!");
+            Log.e("BusinessEditServices", "list_of_services is null!");
         }
         else {
             Log.v("BusinessEditServices", "list of services size: "+list_of_services.size());
@@ -79,6 +82,23 @@ public class BusinessEditServicesActivity extends AppCompatActivity {
         // set layout manager and adapter
         service_list.setLayoutManager(linearLayoutManager);
         service_list.setAdapter(adapter);
+
+        // service list item click listener
+        service_list.addOnItemTouchListener(new RecyclerTouchListener(service_list.getContext(), service_list, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
+
+
+
+
     }
 
 

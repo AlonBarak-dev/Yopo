f7a66f72c4b588c6322db3b0911d56b05cfa318a
alonbarak-dev <alonbarak3110@gmail.com>
2022-12-29 01:51:54 +0200

    Added a username attribute to the Add & remove tasks

--- app/src/main/java/com/example/yopo/data_classes/AddToFirestoreTask.java
+++ app/src/main/java/com/example/yopo/data_classes/AddToFirestoreTask.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import com.google.gson.Gson;
 
@@ -17,21 +18,26 @@ public class AddToFirestoreTask extends AsyncTask<Void, Void, Void> {
 
     private String data;  // Data to be added to Firestore
     private String collectionPath;
+    private String username;
 
     public AddToFirestoreTask(String data) {
         this.data = data;
     }
 
-    public AddToFirestoreTask(HashMap<String, Object> data, String collectionPath) {
+    public AddToFirestoreTask(HashMap<String, Object> data, String collectionPath, String username) {
         Gson gson = new Gson();
         this.data = gson.toJson(data);
         this.collectionPath = collectionPath;
+        this.username = username;
     }
 
     @Override
     protected Void doInBackground(Void... voids) {
         try {
-            URL url = new URL(FUNCTION_URL + "?collection=" + collectionPath);
+            String query = String.format("collection=%s&username=%s",
+                    URLEncoder.encode(collectionPath, "UTF-8"),
+                    URLEncoder.encode(username, "UTF-8"));
+            URL url = new URL(FUNCTION_URL + "?" + query);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setDoOutput(true);
             connection.setRequestMethod("POST");
--- app/src/main/java/com/example/yopo/data_classes/RemoveFromFirestoreTask.java
+++ app/src/main/java/com/example/yopo/data_classes/RemoveFromFirestoreTask.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import com.google.gson.Gson;
 
@@ -15,20 +16,26 @@ public class RemoveFromFirestoreTask extends AsyncTask<Void, Void, Void>{
 
     private String data;  // document key to be removed to Firestore
     private String collectionPath;
+    private String username;
 
     public RemoveFromFirestoreTask(String data) {
         this.data = data;
     }
 
-    public RemoveFromFirestoreTask(String data, String collectionPath) {
+    public RemoveFromFirestoreTask(String data, String collectionPath, String username) {
         this.data = data;
         this.collectionPath = collectionPath;
+        this.username = username;
+
     }
 
     @Override
     protected Void doInBackground(Void... voids) {
         try {
-            URL url = new URL(FUNCTION_URL + "?collection=" + collectionPath);
+            String query = String.format("collection=%s&username=%s",
+                    URLEncoder.encode(collectionPath, "UTF-8"),
+                    URLEncoder.encode(username, "UTF-8"));
+            URL url = new URL(FUNCTION_URL + "?" + query);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setDoOutput(true);
             connection.setRequestMethod("POST");

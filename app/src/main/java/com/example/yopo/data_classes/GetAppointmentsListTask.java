dd527b711e20eff9c23be9d9b781405c23db59ed
alonbarak-dev <alonbarak3110@gmail.com>
2022-12-29 01:09:00 +0200

    Added a class that can retrieve the appointments of a user base on its username, the chosen date and its type

--- app/src/main/java/com/example/yopo/data_classes/GetAppointmentsListTask.java
+++ app/src/main/java/com/example/yopo/data_classes/GetAppointmentsListTask.java
@@ -1,7 +1,6 @@
 package com.example.yopo.data_classes;
 
 import android.os.AsyncTask;
-import android.util.Log;
 
 import com.google.common.reflect.TypeToken;
 import com.google.gson.Gson;
@@ -10,60 +9,57 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.HashMap;
-
-public class GetFromFirestoreTask extends AsyncTask<Void, Void, Void>{
-    private static final String FUNCTION_URL = "https://us-central1-yopo-6aaec.cloudfunctions.net/getFromFirestore";
-
-    private String username;  // document key to be removed to Firestore
-    private String collectionPath;
-
-    public GetFromFirestoreTask(String data) {
-        this.username = data;
-    }
+import java.util.List;
+import java.util.Map;
 
 
-    // For a given username, return the
-    public GetFromFirestoreTask(String data, String collectionPath) {
-        this.username = data;
+public class GetAppointmentsListTask extends AsyncTask<Void, Void, List<HashMap<String, Object>>> {
+    private static final String FUNCTION_URL = "https://us-central1-your-project-id.cloudfunctions.net/getDocumentData";
+
+    private String collectionPath;  // Collection path
+    private String username;
+    private String date;
+    private boolean isClient;
+
+    public GetAppointmentsListTask(String collectionPath, String username, String date, boolean isClient) {
         this.collectionPath = collectionPath;
+        this.username = username;
+        this.date = date;
+        this.isClient = isClient;
     }
 
     @Override
-    protected Void doInBackground(Void... voids) {
+    protected List<HashMap<String, Object>> doInBackground(Void... voids) {
         try {
-            // define the query
-            String query = String.format("collection=%s&document=%s",
+            String query = String.format("collection=%s&username=%s&date=%s&isclient=%s",
                     URLEncoder.encode(collectionPath, "UTF-8"),
-                    URLEncoder.encode(username, "UTF-8"));
-
+                    URLEncoder.encode(username, "UTF-8"),
+                    URLEncoder.encode(date, "UTF-8"),
+                    isClient);
             URL url = new URL(FUNCTION_URL + "?" + query);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
 
             int responseCode = connection.getResponseCode();
             if (responseCode == 200) {
-                // read the response from the server and convert it to string
                 String response = readStream(connection.getInputStream());
                 Gson gson = new Gson();
-                Type type = new TypeToken<HashMap<String, Object>>(){}.getType();
-                // return the HashMap that the server returned
+                Type type = new TypeToken<List<HashMap<String, Object>>>(){}.getType();
                 return gson.fromJson(response, type);
-            }  else {
+            } else {
                 throw new IOException("Error calling function: HTTP response code ${responseCode}");
             }
         } catch (IOException e) {
             e.printStackTrace();
+            return null;
         }
-        return null;
     }
 
-    // This method receive an InputStream and output the String from it
     private String readStream(InputStream stream) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
         StringBuilder result = new StringBuilder();
---
+++ app/src/main/java/com/example/yopo/data_classes/GetByUsernameFromFirestoreTask.java
@@ -0,0 +1,74 @@
+package com.example.yopo.data_classes;
+
+import android.os.AsyncTask;
+
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+
+public class GetByUsernameFromFirestoreTask extends AsyncTask<Void, Void, HashMap<String, Object>>{
+    private static final String FUNCTION_URL = "https://us-central1-yopo-6aaec.cloudfunctions.net/getFromFirestore";
+
+    private String username;  // document key to be removed to Firestore
+    private String collectionPath;
+
+    public GetByUsernameFromFirestoreTask(String data) {
+        this.username = data;
+    }
+
+
+    // For a given username
+    public GetByUsernameFromFirestoreTask(String data, String collectionPath) {
+        this.username = data;
+        this.collectionPath = collectionPath;
+    }
+
+    @Override
+    protected HashMap<String, Object> doInBackground(Void... voids) {
+        try {
+            // define the query
+            String query = String.format("collection=%s&document=%s",
+                    URLEncoder.encode(collectionPath, "UTF-8"),
+                    URLEncoder.encode(username, "UTF-8"));
+
+            URL url = new URL(FUNCTION_URL + "?" + query);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == 200) {
+                // read the response from the server and convert it to string
+                String response = readStream(connection.getInputStream());
+                Gson gson = new Gson();
+                Type type = new TypeToken<HashMap<String, Object>>(){}.getType();
+                // return the HashMap that the server returned
+                return gson.fromJson(response, type);
+            }  else {
+                throw new IOException("Error calling function: HTTP response code ${responseCode}");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // This method receive an InputStream and output the String from it
+    private String readStream(InputStream stream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        StringBuilder result = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            result.append(line);
+        }
+        return result.toString();
+    }
+}

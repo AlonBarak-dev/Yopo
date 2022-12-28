cba3193ba72e4ea5d2f1fbf82596191bc1bd3b8f
alonbarak-dev <alonbarak3110@gmail.com>
2022-12-28 18:05:55 +0200

    Created a Message class for the Client-server communication.

---
+++ app/src/main/java/com/example/yopo/util_classes/Message.java
@@ -0,0 +1,24 @@
+package com.example.yopo.util_classes;
+
+import java.util.HashMap;
+
+public class Message {
+
+    private String ques;
+    private HashMap<String, Object> args;
+
+
+    public Message(String ques, HashMap<String, Object> args) {
+        this.ques = ques;
+        this.args = args;
+    }
+
+    public HashMap<String, Object> get_args(){
+        return this.args;
+    }
+
+    public String get_ques(){
+        return this.ques;
+    }
+
+}
